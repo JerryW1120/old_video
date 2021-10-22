b666b188fbcf19e02bf27f6a8b91170167a8cd55
Jerry9403 <940341746@qq.com>
2021-10-23 00:40:03 +0800

    可以循环处理文件夹下的所有文件了，并且对于文件种类也可以判断

--- change_formatting.py
+++ change_formatting.py
@@ -1,2 +1,10 @@
 import os
-os.popen('ffmpeg -i /Users/jerryw/Desktop/文档/老电影修复/dpx文件/000126720.dpx /Users/jerryw/Desktop/文档/老电影修复/dpx文件/11.mp4')
+
+
+dir = "/Users/jerryw/Desktop/文档/老电影修复/dpx文件/"
+directory = os.listdir(dir)
+
+for file in directory:
+    
+    if str(file).split(".")[-1] == "dpx":
+        os.popen(f'ffmpeg -i {dir}{file} {dir}{str(file).split(".")[0]}.avi')
