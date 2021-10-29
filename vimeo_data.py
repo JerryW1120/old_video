cc20feb73711e1efc5638d7f2812eb1d1eb0f298
Jerry <940341746@qq.com>
2021-10-29 20:56:51 +0800

    改了文件路径，想让修复和原始同时复制

--- vimeo_data.py
+++ vimeo_data.py
@@ -8,9 +8,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 from skimage.metrics import peak_signal_noise_ratio
 
-input_dir = "D:\\test"
-output_dir = "D:\\"
-all_file = os.listdir(input_dir)
+input_dir_origin = "G:\原始2png"
+output_dir_orngin = "D:\原始2分场景"
+input_dir_fixed = "G:\修复2png"
+output_dir_fixed = "D:\修复2分场景"
+all_file = os.listdir(input_dir_origin)
 all_file.sort()
 
 
@@ -18,12 +20,12 @@ def split_fold(baseline, count, png_list):
     png_list.append(all_file[baseline])
     while 1:        
         if count >= len(all_file):#文件尾问题解决
-            if len(png_list) == 0:
+            if len(png_list) == 1:
                 print("已到达文件结尾，分类结束")
-                msg = MIMEText('测试结束了\n\n这是系统自动发出邮件，请不要回复。', 'plain', 'utf-8')
+                msg = MIMEText('原始2和修复2搞定了\n\n这是系统自动发出邮件，请不要回复。', 'plain', 'utf-8')
                 msg['From'] = '940341746@qq.com'
                 msg['To'] = '940341746@qq.com'
-                msg['Subject'] = '测试结束'
+                msg['Subject'] = '原始2和修复2搞定了'
                 from_addr = '940341746@qq.com'
                 # 密码是授权码
                 password = 'szowquehfogxbgaa'
@@ -43,8 +45,8 @@ def split_fold(baseline, count, png_list):
             else:
                 return count, baseline, png_list
         else:           
-            cmpr1 = plt.imread(os.path.join(input_dir, all_file[count - 1]))
-            cmpr2 = plt.imread(os.path.join(input_dir, all_file[count]))
+            cmpr1 = plt.imread(os.path.join(input_dir_origin, all_file[count - 1]))
+            cmpr2 = plt.imread(os.path.join(input_dir_origin, all_file[count]))
             cmpr1 = np.array(cmpr1)
             cmpr2 = np.array(cmpr2)
             psnr = peak_signal_noise_ratio(cmpr1, cmpr2)##用cv的库要读图片(done)
@@ -83,10 +85,15 @@ while count <= len(all_file):
     img_list = answer[2]
 
 
-    folder_dir = os.path.join(output_dir, str(img_list[0].split(".")[0]) + ' - ' + str(img_list[-1].split(".")[0]))
-    os.makedirs(folder_dir)
-    print("已创建文件夹%s" % (folder_dir))
+    folder_dir_origin = os.path.join(output_dir_orngin, str(img_list[0].split(".")[0]) + ' - ' + str(img_list[-1].split(".")[0]))
+    os.makedirs(folder_dir_origin)
+    print("已创建文件夹%s" % (folder_dir_origin))
+
+    folder_dir_fixed = os.path.join(output_dir_fixed, str(img_list[0].split(".")[0]) + ' - ' + str(img_list[-1].split(".")[0]))
+    os.makedirs(folder_dir_fixed)
+    print("已创建文件夹%s" % (folder_dir_fixed))
             
     for file in answer[2]:
-        copyfile(input_dir + '\\' + str(file), folder_dir + '\\' + str(file))
+        copyfile(input_dir_origin + '\\' + str(file), folder_dir_origin + '\\' + str(file))
+        copyfile(input_dir_fixed + '\\' + str(file), folder_dir_fixed + '\\' + str(file))
     
