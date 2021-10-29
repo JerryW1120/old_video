87ea56e277343feb75be45c0a0f6fd3df45912e5
Jerry <940341746@qq.com>
2021-10-29 20:24:07 +0800

    这才是邮件推送的完全体

--- test_something.py
+++ test_something.py
@@ -1,7 +1,22 @@
-import os
-
-input_dir = "/Users/jerryw/Desktop/文档/老电影修复/dpx文件"
-all_file = os.listdir(input_dir)
-all_file.sort()
-
-print(all_file)
+import smtplib
+from email.mime.text import MIMEText
+# 构造邮件
+msg = MIMEText('您的程序已执行完毕，请及时上线检查！\n\n这是系统自动发出邮件，请不要回复。', 'plain', 'utf-8')
+msg['From'] = '940341746@qq.com'
+msg['To'] = '940341746@qq.com'
+msg['Subject'] = '您的程序已执行完毕！'
+from_addr = '940341746@qq.com'
+# 密码是授权码
+password = 'szowquehfogxbgaa'
+to_addr = '940341746@qq.com'
+smtp_server = 'smtp.qq.com'
+# QQ邮箱的SMTP服务需SSL加密，端口为465
+server = smtplib.SMTP_SSL(smtp_server)
+# 显示发送过程
+server.set_debuglevel(1)
+# 登陆验证
+server.login(from_addr, password)
+# 发送邮件
+server.sendmail(from_addr, [to_addr], msg.as_string())
+# 退出
+server.quit()
