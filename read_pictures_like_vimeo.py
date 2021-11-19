4ac199549de5748b2ea898ac892842387a2dd388
Jerry9403 <940341746@qq.com>
2021-11-19 20:48:51 +0800

    去掉大循环，每次返回七组图片

--- read_pictures_like_vimeo.py
+++ read_pictures_like_vimeo.py
@@ -6,88 +6,73 @@ import numpy as np
 '''
 返回值是三级列表
 [ [[og_1,fixed_1], [og_2,fixed_2]...[og_7,fixed_7]]   [[], [], ...[]]   ...]
+
+如果去掉大循环，每次返回一个二级列表，大循环需要自己设置
+[[og_1,fixed_1], [og_2,fixed_2]...[og_7,fixed_7]]
 '''
 def input_pictures_with_pairs(dir_origin, dir_fixed):
+    # 去掉大循环时，路径具体到场景的文件夹(D:\修复2分场景\\000109449 - 000109497)
+    # 有大循环时，路径具体到总文件夹(D:\修复2分场景)
 
-    folders = os.listdir(dir_origin)
-    folders.sort()
-
-    '''
-    读取各个场景下的文件夹
-    '''
-    for folder in folders:
-        files = os.listdir(dir_origin + '\\' + folder)
-        files.sort()
+    files = os.listdir(dir_origin)
+    files.sort()
 
     count = 0
-    whole_list = []
+    middle_list = []
+    count_inside = count
 
-    while count <= len(files) - 7:
-        middle_list = []
-        count_inside = count
+    while count_inside - count < 7:
+        pair_list = []
+        pic_og = plt.imread(os.path.join(dir_origin, files[count_inside]))
+        pic_fixed = plt.imread(os.path.join(dir_fixed, files[count_inside]))
+        pic_og = np.array(pic_og)
+        pic_fixed = np.array(pic_fixed)
+        
+        pair_list.append(pic_og)
+        pair_list.append(pic_fixed)
+        middle_list.append(pair_list)
+        count_inside += 1
 
-        while count_inside - count < 7:
-            pair_list = []
-            pic_og = plt.imread(os.path.join(dir_origin, folder, files[count_inside]))
-            pic_fixed = plt.imread(os.path.join(dir_fixed, folder, files[count_inside]))
-            pic_og = np.array(pic_og)
-            pic_fixed = np.array(pic_fixed)
-            
-            pair_list.append(pic_og)
-            pair_list.append(pic_fixed)
-            middle_list.append(pair_list)
-            count_inside += 1
-
-        whole_list.append(middle_list)
-        count += 1
     
-    return whole_list
+    return middle_list
 
 
 '''
 返回值是两个二级列表
 [ [og_1, og_2, og_3, ... og_7], ...[] ]
 [ [fixed_1, fixed_2, fixed_3, ... fixed_7], ...[] ]
+
+如果去掉大循环，每次返回两个普通列表，大循环需要自己设置
+[og_1, og_2, og_3, ... og_7]
+[fixed_1, fixed_2, fixed_3, ... fixed_7]
 '''
 def input_pictures_with_two_lists(dir_origin, dir_fixed):
+    # 去掉大循环时，路径具体到场景的文件夹(D:\修复2分场景\\000109449 - 000109497)
+    # 有大循环时，路径具体到总文件夹(D:\修复2分场景)
 
-    folders = os.listdir(dir_origin)
-    folders.sort()
-
-    '''
-    读取各个场景下的文件夹
-    '''
-    for folder in folders:
-        files = os.listdir(dir_origin + '\\' + folder)
-        files.sort()
+    files = os.listdir(dir_origin)
+    files.sort()
 
     count = 0
-    whole_og_list = []
-    whole_fixed_list = []
 
-    while count <= len(files) - 7:
-        og_list = []
-        fixed_list = []
-        count_inside = count
 
-        while count_inside - count < 7:
-            pic_og = plt.imread(os.path.join(dir_origin, folder, files[count_inside]))
-            pic_fixed = plt.imread(os.path.join(dir_fixed, folder, files[count_inside]))
-            pic_og = np.array(pic_og)
-            pic_fixed = np.array(pic_fixed)
-        
-            og_list.append(pic_og)
-            fixed_list.append(pic_fixed)
-            count_inside += 1
+    og_list = []
+    fixed_list = []
+    count_inside = count
 
-        whole_og_list.append(og_list)
-        whole_fixed_list.append(fixed_list)
-
-        count += 1
+    while count_inside - count < 7:
+        pic_og = plt.imread(os.path.join(dir_origin, files[count_inside]))
+        pic_fixed = plt.imread(os.path.join(dir_fixed, files[count_inside]))
+        pic_og = np.array(pic_og)
+        pic_fixed = np.array(pic_fixed)
     
-    return whole_og_list, whole_fixed_list
+        og_list.append(pic_og)
+        fixed_list.append(pic_fixed)
+        count_inside += 1
 
-#如果要每7个7个输入网络的话，去掉大循环即可
+ 
+    return og_list, fixed_list
+
 
 
            
