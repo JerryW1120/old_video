152b8a6c7e35ef4086c1aa4da23e6f6cbd583d43
Jerry9403 <940341746@qq.com>
2021-11-19 19:49:25 +0800

    设置为传参类型的，等会儿试一试间接调用

--- read_pictures_like_vimeo.py
+++ read_pictures_like_vimeo.py
@@ -3,79 +3,89 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-dir_origin = "D:\原始2分场景"
-dir_fixed = "D:\修复2分场景"
-folders = os.listdir(dir_origin)
-folders.sort()
-
-'''
-读取各个场景下的文件夹
-'''
-for folder in folders:
-        files = os.listdir(dir_origin + '\\' + folder)
-        files.sort()
-
 '''
 返回值是三级列表
 [ [[og_1,fixed_1], [og_2,fixed_2]...[og_7,fixed_7]]   [[], [], ...[]]   ...]
 '''
-def input_pictures_with_pairs():
+def input_pictures_with_pairs(dir_origin, dir_fixed):
+
+    folders = os.listdir(dir_origin)
+    folders.sort()
+
+    '''
+    读取各个场景下的文件夹
+    '''
+    for folder in folders:
+        files = os.listdir(dir_origin + '\\' + folder)
+        files.sort()
+
+    count = 0
+    whole_list = []
+
+    while count <= len(files) - 7:
+        middle_list = []
+        count_inside = count
+
+        while count_inside - count < 7:
+            pair_list = []
+            pic_og = plt.imread(os.path.join(dir_origin, folder, files[count_inside]))
+            pic_fixed = plt.imread(os.path.join(dir_fixed, folder, files[count_inside]))
+            pic_og = np.array(pic_og)
+            pic_fixed = np.array(pic_fixed)
+            
+            pair_list.append(pic_og)
+            pair_list.append(pic_fixed)
+            middle_list.append(pair_list)
+            count_inside += 1
+
+        whole_list.append(middle_list)
+        count += 1
     
-        count = 0
+    return whole_list
 
-        whole_list = []
-        while count <= len(files) - 7:
-            middle_list = []
-            count_inside = count
 
-            while count_inside - count < 7:
-                pair_list = []
-                pic_og = plt.imread(os.path.join(dir_origin, folder, files[count_inside]))
-                pic_fixed = plt.imread(os.path.join(dir_fixed, folder, files[count_inside]))
-                pic_og = np.array(pic_og)
-                pic_fixed = np.array(pic_fixed)
-                
-                pair_list.append(pic_og)
-                pair_list.append(pic_fixed)
-                middle_list.append(pair_list)
-                count_inside += 1
-
-            whole_list.append(middle_list)
-            count += 1
-        
-        return whole_list
 '''
 返回值是两个二级列表
-[[og_1, og_2, og_3, ... og_7], ...[] ]
-[[fixed_1, fixed_2, fixed_3, ... fixed_7], ...[]]
+[ [og_1, og_2, og_3, ... og_7], ...[] ]
+[ [fixed_1, fixed_2, fixed_3, ... fixed_7], ...[] ]
 '''
-def input_pictures_with_two_lists():
+def input_pictures_with_two_lists(dir_origin, dir_fixed):
 
-        count = 0
+    folders = os.listdir(dir_origin)
+    folders.sort()
 
-        whole_og_list = []
-        whole_fixed_list = []
-        while count <= len(files) - 7:
-            og_list = []
-            fixed_list = []
-            count_inside = count
+    '''
+    读取各个场景下的文件夹
+    '''
+    for folder in folders:
+        files = os.listdir(dir_origin + '\\' + folder)
+        files.sort()
 
-            while count_inside - count < 7:
-                pic_og = plt.imread(os.path.join(dir_origin, folder, files[count_inside]))
-                pic_fixed = plt.imread(os.path.join(dir_fixed, folder, files[count_inside]))
-                pic_og = np.array(pic_og)
-                pic_fixed = np.array(pic_fixed)
-            
-                og_list.append(pic_og)
-                fixed_list.append(pic_fixed)
-                count_inside += 1
+    count = 0
+    whole_og_list = []
+    whole_fixed_list = []
 
-            whole_og_list.append(og_list)
-            whole_fixed_list.append(fixed_list)
+    while count <= len(files) - 7:
+        og_list = []
+        fixed_list = []
+        count_inside = count
 
-            count += 1
+        while count_inside - count < 7:
+            pic_og = plt.imread(os.path.join(dir_origin, folder, files[count_inside]))
+            pic_fixed = plt.imread(os.path.join(dir_fixed, folder, files[count_inside]))
+            pic_og = np.array(pic_og)
+            pic_fixed = np.array(pic_fixed)
         
-        return whole_og_list, whole_fixed_list
+            og_list.append(pic_og)
+            fixed_list.append(pic_fixed)
+            count_inside += 1
+
+        whole_og_list.append(og_list)
+        whole_fixed_list.append(fixed_list)
+
+        count += 1
+    
+    return whole_og_list, whole_fixed_list
 
 #如果要每7个7个输入网络的话，去掉大循环即可
 
--- read_pictures_like_vimeo_demo.py
+++ read_pictures_like_vimeo_demo.py
@@ -3,10 +3,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-dir_origin = "D:\原始2分场景\\000109449 - 000109497"
-dir_fixed = "D:\修复2分场景\\000109449 - 000109497"
-files = os.listdir(dir_origin)
-files.sort()
+
+
 
 '''
 读取各个场景下的文件夹
@@ -19,73 +17,79 @@ files.sort()
 返回值是三级列表
 [ [[og_1,fixed_1], [og_2,fixed_2]...[og_7,fixed_7]]   [[], [], ...[]]   ...]
 '''
-def input_pictures_with_pairs():
+def input_pictures_with_pairs(dir_origin, dir_fixed):
+
+    files = os.listdir(dir_origin)
+    files.sort()
     
-        count = 0
+    count = 0
 
-        whole_list = []
-        while count <= len(files) - 7:
-            middle_list = []
-            count_inside = count
+    whole_list = []
+    while count <= len(files) - 7:
+        middle_list = []
+        count_inside = count
 
-            while count_inside - count < 7:
-                pair_list = []
-                # pic_og = plt.imread(os.path.join(dir_origin, folder, files[count]))
-                # pic_fixed = plt.imread(os.path.join(dir_fixed, folder, files[count]))
-                # pic_og = np.array(pic_og)
-                # pic_fixed = np.array(pic_fixed)
-                
-                pair_list.append(files[count_inside])
-                pair_list.append(files[count_inside])
-                middle_list.append(pair_list)
-                count_inside += 1
+        while count_inside - count < 7:
+            pair_list = []
+            # pic_og = plt.imread(os.path.join(dir_origin, folder, files[count]))
+            # pic_fixed = plt.imread(os.path.join(dir_fixed, folder, files[count]))
+            # pic_og = np.array(pic_og)
+            # pic_fixed = np.array(pic_fixed)
+            
+            pair_list.append(files[count_inside])
+            pair_list.append(files[count_inside])
+            middle_list.append(pair_list)
+            count_inside += 1
 
-            whole_list.append(middle_list)
-            count += 1
-        
-        return whole_list
+        whole_list.append(middle_list)
+        count += 1
+    
+    return whole_list
 '''
 返回值是两个二级列表
 [[og_1, og_2, og_3, ... og_7], ...[] ]
 [[fixed_1, fixed_2, fixed_3, ... fixed_7], ...[]]
 '''
-def input_pictures_with_two_lists():
+def input_pictures_with_two_lists(dir_origin, dir_fixed):
 
-        count = 0
+    files = os.listdir(dir_origin)
+    files.sort()
 
-        whole_og_list = []
-        whole_fixed_list = []
-        while count <= len(files) - 7:
-            og_list = []
-            fixed_list = []
-            count_inside = count
+    count = 0
+    whole_og_list = []
+    whole_fixed_list = []
 
-            while count_inside - count < 7:
-                # pic_og = plt.imread(os.path.join(dir_origin, folder, files[count]))
-                # pic_fixed = plt.imread(os.path.join(dir_fixed, folder, files[count]))
-                # pic_og = np.array(pic_og)
-                # pic_fixed = np.array(pic_fixed)
-            
-                og_list.append(files[count_inside])
-                fixed_list.append(files[count_inside])
-                count_inside += 1
+    while count <= len(files) - 7:
+        og_list = []
+        fixed_list = []
+        count_inside = count
 
-            whole_og_list.append(og_list)
-            whole_fixed_list.append(fixed_list)
-
-            count += 1
+        while count_inside - count < 7:
+            # pic_og = plt.imread(os.path.join(dir_origin, folder, files[count]))
+            # pic_fixed = plt.imread(os.path.join(dir_fixed, folder, files[count]))
+            # pic_og = np.array(pic_og)
+            # pic_fixed = np.array(pic_fixed)
         
-        return whole_og_list, whole_fixed_list
+            og_list.append(files[count_inside])
+            fixed_list.append(files[count_inside])
+            count_inside += 1
+
+        whole_og_list.append(og_list)
+        whole_fixed_list.append(fixed_list)
+
+        count += 1
+    
+    return whole_og_list, whole_fixed_list
 
 #如果要每7个7个输入网络的话，去掉大循环即可
 
 #print("成对儿返回得到的列表就是：",input_pictures_with_pairs())
-a = input_pictures_with_pairs()
-for i in range(len(a)):
-    for j in range(len(a[i])):
-        print(a[i][j])
+# a = input_pictures_with_pairs()
+# for i in range(len(a)):
+#     for j in range(len(a[i])):
+#         print(a[i][j])
     
-    print('\n')
+#     print('\n')
 
 
 #two_lists = input_pictures_with_two_lists()
