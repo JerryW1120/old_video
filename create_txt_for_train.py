a6a23c2b473221bc4d0b01f1af81a2f0dbe51827
JerryW1120 <940341746@qq.com>
2021-12-10 22:49:47 +0800

    修改了一下index_og

--- create_txt_for_train.py
+++ create_txt_for_train.py
@@ -3,7 +3,7 @@ import read_pictures_like_vimeo_demo as ld
 
 
 
-two_lists = ld.input_pictures_with_two_lists('D:修复2分场景', 'D:\修复2分场景')
+two_lists = ld.input_pictures_with_two_lists('D:\原始2分场景', 'D:\修复2分场景')
 
 print("\n", two_lists)
 
@@ -18,7 +18,7 @@ folders = os.listdir('D:\原始2分场景')
 count = 0
 
 for second_lists in two_lists:
-    path_og = 'fixed2/'
+    path_og = 'og2/'
     path_og = os.path.join(path_og, folders[count]) + '/'
     for lists in second_lists:       
         for item in lists:
